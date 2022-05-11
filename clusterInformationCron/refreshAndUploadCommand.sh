1570675591eab5219bc04e36e120931a812de1b1
Mufazzal <h.mufazzal@gmail.com>
2022-05-11 14:18:59 +0530

    jc1

--- clusterInformationCron/refreshAndUploadCommand.sh
+++ clusterInformationCron/refreshAndUploadCommand.sh
@@ -1,11 +1,12 @@
 echo "<<Request for New K8S join command>>"
-
 joinCommand=$(kubeadm token create --print-join-command)
 
+echo $joinCommand
 aws ssm put-parameter \
-    --name "dev/AWSK8S/workerNodeJoinCommand" \
+    --name "/dev/AWSK8S/workerNodeJoinCommand" \
     --type "String" \
-    --value $joinCommand \
+    --value "$joinCommand" \
+    --region "us-east-1" \
     --overwrite
 
 echo "<<DONE Request for New K8S join command>>"
